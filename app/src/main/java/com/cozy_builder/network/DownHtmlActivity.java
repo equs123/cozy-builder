fd4ce038c785a894fa48a4d201ee94927865a6de
equs123 <equs0223@gmail.com>
2018-05-16 22:10:07 +0900

    첫번째 Commit 두번째 Commit

--- app/src/main/java/com/cozy_builder/network/DownHtmlActivity.java
+++ app/src/main/java/com/cozy_builder/network/DownHtmlActivity.java
@@ -51,7 +51,7 @@ public class DownHtmlActivity extends AppCompatActivity {
                         handler.sendEmptyMessage(0);
                     }
                 });
-                th.start();
+                th.start();//
             }
         });
     }
